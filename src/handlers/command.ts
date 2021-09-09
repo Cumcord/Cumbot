75b7f37081fd790c1907bb38b7dfcc691e49e7ae
rem-my <ethandavidbarrow@outlook.com>
2021-09-09 07:59:51 +0100

    Once again I commit unfinished code

--- src/handlers/command.ts
+++ src/handlers/command.ts
@@ -12,15 +12,20 @@ import { Command } from '../util/definitions'
 export const commands = new Collection<string, Command>();
 
 export default async function init(client: Client) {
-    const commandFolder = path.join(__dirname, '../commands/');
+    const commandFolder = path.join(__dirname, '..dist/', 'commands/');
     const commandFolders = fs.readdirSync(commandFolder);
 
     for (const folder of commandFolders) {
-        const commandFiles = fs.readdirSync(`${commandFolder}/${folder}`).filter((file) => file.endsWith('.ts'))
+        const commandFiles = fs.readdirSync(`${commandFolder}/${folder}`).filter((file) => file.endsWith('.js'))
         
-        for (const file of commandFolder) {
-            const command = require(`${commandFolder}/${folder}/${file}`);
+        for (const file of commandFiles) {
+            const command = (await import(`${commandFolder}/${folder}/${file}`)).default as Command | undefined;
             commands.set(command.name, command);
         }
     }
+    console.log(`Successfully fetched ${commands.values.length} commands.`)
+
+    if (!client.application?.owner) client.application.fetch();
+
+
 }
